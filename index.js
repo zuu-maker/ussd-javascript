02cc6f73d86b33253503b7ad4bd70496877d4a5a
Mkhuzo <zulumkhuzo@gmail.com>
2023-08-05 16:04:54 +0800

    change route name

--- index.js
+++ index.js
@@ -7,44 +7,41 @@ const app = express();
 
 app.use(morganLogger("dev"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/*", (req, res) => {
   res.send("This is me checking out how create a USSD");
 });
 
-app.post("/*", (req, res) => {
-  let { sessionId, serviceCode, phoneNumber, text } = req.body;
+app.post("/ussd", (req, res) => {
+  // Read the variables sent via POST from our API
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+
+  let response = "nothing";
+
   if (text == "") {
     // This is the first request. Note how we start the response with CON
-    let response = `CON What would you want to check
-        1. My Account
+    response = `CON What would you like to check
+        1. My account
         2. My phone number`;
-    res.send(response);
   } else if (text == "1") {
     // Business logic for first level response
-    let response = `CON Choose account information you want to view
-        1. Account number
-        2. Account balance`;
-    res.send(response);
+    response = `CON Choose account information you want to view
+        1. Account number`;
   } else if (text == "2") {
     // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`;
-    res.send(response);
+    // This is a terminal request. Note how we start the response with END
+    response = `END Your phone number is ${phoneNumber}`;
   } else if (text == "1*1") {
-    // Business logic for first level response
-    let accountNumber = "ACC1001";
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`;
-    res.send(response);
-  } else if (text == "1*2") {
     // This is a second level response where the user selected 1 in the first instance
-    let balance = "NGN 10,000";
+    const accountNumber = "ACC100101";
     // This is a terminal request. Note how we start the response with END
-    let response = `END Your balance is ${balance}`;
-    res.send(response);
-  } else {
-    res.status(400).send("Bad request!");
+    response = `END Your account number is ${accountNumber}`;
   }
+
+  // Send the response back to the API
+  res.set("Content-Type: text/plain");
+  res.send(response);
 });
 
 app.listen(port, () => {
